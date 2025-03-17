2a12b21978871421770e61623e5aa7cc8073ff4e
Navya Vijayakumar <navyavijayakumar3@gmail.com>
2025-03-17 12:36:40 +0530

    initial commit

---
+++ swapping.js
@@ -0,0 +1,17 @@
+//write a program to swap 2 numbers
+var num1=1
+var num2=2
+var num3=0
+
+num3=num1
+num1=num2
+num2=num3
+console.log(`num1=${num1} num2=${num2}`);
+
+//write a program to swap 2 numbers without using 3 rd variable
+var num1=1
+var num2=2
+num1=num1+num2
+num2=num1-num2
+num1=num1-num2
+console.log(`num1=${num1} num2=${num2}`);
---
+++ variables.js
@@ -0,0 +1,7 @@
+//your sbk savings account with acno SBK12345 current balance is 35000
+var bankName="SBK"
+var accountNumber="SBK12345"
+var accountType="savings"
+var accountBalance=35000
+//console.log("Your",bankName,accountType,"account with acno",accountNumber,"current balance is",accountBalance)
+console.log(`your ${bankName} ${accountType} account with acno ${accountNumber} current balance is ${accountBalance}`);
