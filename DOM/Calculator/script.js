b120a8e10935ad1fd831f54a387f16b539f09246
Navya Vijayakumar <navyavijayakumar3@gmail.com>
2025-03-29 22:19:18 +0530

    calculator completed

--- DOM/Calculator/script.js
+++ DOM/Calculator/script.js
@@ -1,3 +1,51 @@
-function displayNumber(number){
+function liveScreen(displayText){
+    //fetch textbox  current value
+    let currentValue=display.value
+    // valid op array
+    const operators=["+","-","*","/"]
+    if(currentValue.length==0 && displayText=="-"){
+    display.value+=displayText
+    }
+
+    else if(currentValue.length>0 && operators.includes(displayText)){
+        let lastCharacter=currentValue.charAt(currentValue.length-1)
+
+        if(operators.includes(lastCharacter)){
+            currentValue=currentValue.slice(0,-1)
+            currentValue+=displayText
+            display.value=currentValue
+        }
+        else{
+
+        
+            display.value+=displayText
+    
+        }
+
+    }
+
+    else{
+        if(currentValue.length==0 && operators.includes(displayText)){
+            return
+        }
+        display.value+=displayText
+
+    }
     
 }
+
+function clearBox(){
+    display.value=""
+    
+}
+function evaluateExpression(){
+    let expr=display.value
+    let result=eval(expr)
+    display.value=result
+}
+function backSpace(){
+    let currentValue=display.value
+    let result=currentValue.slice(0,-1)
+    display.value=result
+}
+
